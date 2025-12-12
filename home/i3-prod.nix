f17a31acce45ac81afede51e47768ed420bbc6d1
Michal Walewski <pelikan2333@proton.me>
2025-12-12 22:09:32 +0100

    hm: refine i3-prod config

--- home/i3-prod.nix
+++ home/i3-prod.nix
@@ -1,6 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  # Pakiety potrzebne do i3
+  home.packages = with pkgs; [
+    rofi
+    nitrogen
+    dmenu
+  ];
+
   xsession = {
     enable = true;
 
@@ -11,9 +18,9 @@
         modifier = "Mod4";
         terminal = "wezterm";
 
-        # minimalne, bezpieczne skróty
         keybindings = {
           "Mod4+Return" = "exec wezterm";
+          "Mod4+d" = "exec rofi -show drun";
           "Mod4+Shift+q" = "kill";
           "Mod4+Shift+r" = "restart";
         };
