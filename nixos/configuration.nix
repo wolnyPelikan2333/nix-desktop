14387dc3d91efa095602d7c05a6bb932c5fab171
Michal Walewski <pelikan2333@proton.me>
2025-12-12 19:18:15 +0100

    auto: switch + snapshot 2025-12-12_19-18

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,11 +59,6 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-
-  services.xserver.windowManager.i3.enable = true;
-  programs.hyprland.enable = true;
-
-
   services.xserver.xkb = {
     layout = "pl";
     variant = "";
