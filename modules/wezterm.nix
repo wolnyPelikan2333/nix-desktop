d30a4cefaa5ee08abe811c04f909e8f14eb2cfde
michal walewski <pelikan2333@proton.me>
2025-12-19 10:48:35 +0100

    update modules/wezterm.nix && docs/\305\233ci\304\205gi/wezterm/keybindings.md

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -38,12 +38,40 @@
       -- Nowa karta: Ctrl+A c
       { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 
+       -- Poprzednia / następna karta
+      { key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+      { key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+
+       -- Zmień nazwę karty
+    { key = ",", mods = "LEADER", action = act.PromptInputLine {
+        description = "Rename tab:",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
+    },
+
+    -- Zamknij kartę
+    { key = "&", mods = "LEADER", action = act.CloseCurrentTab({ confirm = true }) },
+
       -- Zamknij panel: Ctrl+A x
       { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
 
       -- Powiększ panel: Ctrl+A Space
       { key = " ", mods = "LEADER", action = act.TogglePaneZoomState },
     }
+
+    -- Przełączanie kart: Ctrl+A 1..9
+	for i = 1, 9 do
+	  table.insert(config.keys, {
+	    key = tostring(i),
+	    mods = "LEADER",
+	    action = act.ActivateTab(i - 1),
+	  })
+       end
+
 
     return config
   '';
